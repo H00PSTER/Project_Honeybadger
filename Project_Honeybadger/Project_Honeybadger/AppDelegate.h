043b8d26c7d18e3c7348d0e8be3f8baa3ddd373b
Chris Yerina <chrisyerina@pcp114581pcs.wireless.calpoly.edu>
2013-10-29 17:10:59 -0700

    first commit

---
+++ Project_Honeybadger/Project_Honeybadger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project_Honeybadger
+//
+//  Created by Chris Yerina on 10/29/13.
+//  Copyright (c) 2013 Chris Yerina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project_Honeybadger/Project_Honeybadger/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project_Honeybadger
+//
+//  Created by Chris Yerina on 10/29/13.
+//  Copyright (c) 2013 Chris Yerina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
