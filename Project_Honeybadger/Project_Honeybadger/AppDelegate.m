a800966e65eb72de2cee495dc9e6958a2af7051f
Stephen Kobata <Syntax@Stephens-MacBook-Air.local>
2013-10-30 19:34:28 -0700

    Stephen

--- Project_Honeybadger/Project_Honeybadger/AppDelegate.m
+++ Project_Honeybadger/Project_Honeybadger/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"Stephen Is the Best");
+    NSLog(@"Stephen Is the Best!!!");
     NSLog(@"Andy can understand Github");
     return YES;
 }
