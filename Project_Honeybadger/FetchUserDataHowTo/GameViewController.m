d9fed60d8eb47a7ba6864f98c6ae60f9289feced
MrQuicksilver <mzehr@232x02.cp-calpoly.edu>
2013-11-21 18:03:18 -0800

    Added code that updates location

--- Project_Honeybadger/FetchUserDataHowTo/GameViewController.m
+++ Project_Honeybadger/FetchUserDataHowTo/GameViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "GameViewController.h"
-
+#import <CoreLocation/CoreLocation.h>
 @interface GameViewController ()
-
+@property CLLocationManager *locationManager;
 @end
 
 @implementation GameViewController
+- (IBAction)startTracking:(id)sender {
+    [self.locationManager startUpdatingLocation];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -22,11 +25,25 @@
     }
     return self;
 }
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    CLLocationCoordinate2D currentCoordinates = newLocation.coordinate;
+    NSLog(@"Entered new Location with the coordinates Latitude: %f Longitude: %f", currentCoordinates.latitude, currentCoordinates.longitude);
+}
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"Unable to start location manager. Error:%@", [error description]);
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.locationManager = [[CLLocationManager alloc] init];
+    [self.locationManager setDelegate:self];
+    //Only applies when in foreground otherwise it is very significant changes
+    [self.locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+    self.locationManager.distanceFilter = 15;
 	// Do any additional setup after loading the view.
+    [self.locationManager startUpdatingLocation];
+
 }
 
 - (void)didReceiveMemoryWarning
